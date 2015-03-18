d35add4a19627be9b06cb0498a7088ec774c51e5
Reece Tucker <reecetucker2@gmail.com>
2015-03-18 14:47:39 +0000

    edititem.php edited online with Bitbucket

--- edititem.php
+++ edititem.php
@@ -1,24 +1,34 @@
 <?php
-$uid = "i7214451"; 
-$pwd = "Password"; 
-$host = "127.0.0.1";
-$db = $uid;
-$item_id=13;
-$conn = mysqli_connect($host, $uid, $pwd, $db);
+require($_SERVER['DOCUMENT_ROOT'] . '/assignment2/src/require.php');
+$conn = Common::connect_db();
 
 if (isset($_POST["confirm"])){
 	$name = $_POST['itemName'];
+	$name = Common::clean($name, $conn);
 	$desc = $_POST['itemDesc'];
+	$desc = Common::clean($desc, $conn);
 	$tags = $_POST['itemTags'];
+	$tags = Common::clean($tags, $conn);
 	$updateqry = "UPDATE ITEM SET ITEM_NAME='$name', ITEM_DESC='$desc' WHERE ITEM_ID = '$item_id';";
 	mysqli_query($conn, $updateqry);
 	
+	$name = clean ($name, $conn);
+	$desc = clean ($desc, $conn);
+	$tags = clean ($tags, $conn);
+	
 	$arr = Array();
 	$arr=explode(" ",$tags);
-
+	
 	foreach ($arr as $temp_tag){
 		$updateqry2 ="INSERT INTO TAG(ITEM_ID, TAG) VALUES($item_id,'$temp_tag');";
 		mysqli_query($conn, $updateqry2);
+	}
+	if(isset($_POST["deletebox"])){	
+		$updateqry="UPDATE ITEM SET IS_OBSELETE =1 WHERE ITEM_ID= $item_id;";
+		mysqli_query($conn, $updateqry);
+	}else{
+		$updateqry="UPDATE ITEM SET IS_OBSELETE =0 WHERE ITEM_ID= $item_id;";
+		mysqli_query($conn, $updateqry);
 	}
 }
 
@@ -42,9 +52,25 @@ echo"<form name='adimAdd' action='edititem.php'  method='Post' enctype='multipar
 while ($result = mysqli_fetch_array($result,MYSQLI_ASSOC)){
 	$n = $result['ITEM_NAME'];
 	$d = $result['ITEM_DESC'];
-	echo"<input type='text' name='itemName'value ='$n'><br>";
-	echo"<input type='text' name='itemDesc'value ='$d'><br>";
-	echo"<input type='text' name='itemTags'value ='$tag_string'><br>";
+	$del = $result['IS_OBSELETE'];
+	echo"<input required type='text' name='itemName' maxlength = '30' value ='$n'><br>";
+	echo"<input required type='text' name='itemDesc' maxlength = '140' value ='$d'><br>";
+	echo"<input required type='text' name='itemTags' maxlength = '200' value ='$tag_string'><br>";
+	echo"Obselete";
+			if($del==1){
+				echo"<input type ='checkbox' name ='deletebox'  checked >";
+			}else{
+				echo"<input type ='checkbox' name ='deletebox'>";
+			}
 }
 echo "<button type='submit' name = 'confirm'> Add Product</button>";
+
+
+	function clean($info, $conn){
+					$info = trim($info);
+					$info = strip_Tags($info);
+					$info = mysqli_real_escape_string($conn, $info);
+					return $info;
+				}
+
 ?>
