5555a11297a7cfa831120c9eb9e6ba6d34a77541
Reece Tucker <reecetucker2@gmail.com>
2015-03-18 14:45:28 +0000

    addvariant.php edited online with Bitbucket

--- addvariant.php
+++ addvariant.php
@@ -1,21 +1,24 @@
 <?php
+require($_SERVER['DOCUMENT_ROOT'] . '/assignment2/src/require.php');
 session_start();
-$feedback = $_SESSION["check"]; // Note from bradley - Could this "Check" be changed into something less general, could be "v_added" = true, etc.
+$feedback = $_SESSION["check"];
 if ($feedback==true){
 echo "Variant has been added";
 }
 
-$item_id = $_SESSION["itemID"];
-	$uid = "i7214451"; 
-	$pwd = "Password"; 
-	$host = "127.0.0.1";
-	$db = $uid;
-	$conn = mysqli_connect($host, $uid, $pwd, $db);
-echo $item_id;
+	$item_id = $_SESSION["itemID"];
+	$conn = Common::connect_db();
+	if(isset($_POST["confirm"])){
+	
+	echo $item_id;
 	$variantDesc =$_POST['variantDesc'];
+	$variantDesc = Common::clean($variantDesc, $conn);
 	$price =$_POST['price'];
+	$price = Common::clean($price, $conn);
 	$stock = $_POST['stock'];
-
+	$stock = Common::clean($stock, $conn);
+	$count = 1;
+	
 $query = "SELECT * FROM ITEM WHERE ITEM_ID = $item_id;";
 $result = mysqli_query($conn, $query);
 	if($result){
@@ -27,14 +30,17 @@ $result = mysqli_query($conn, $query);
 	}else{
 		echo"error couldn't get the details of this item ";
 	}
-	if (isset($_POST["variantConfirm"])){
-	$query4 = "SELECT MAX(VARIANT_ID) FROM ITEM_VARIANT WHERE ITEM_ID = ($item_id);"; 
+	
+	$query4 = "SELECT MAX(VARIANT_ID) FROM ITEM_VARIANT WHERE ITEM_ID = $item_id;"; 
 	$res4 = mysqli_query($conn, $query4);
 	$row= mysqli_fetch_row($res4);
 	$var_Id =$row[0]+1;
 //print values of item (non-editable)
-$query2 = "INSERT INTO ITEM_VARIANT(VARIANT_ID,ITEM_ID,VARIANT_DESC, PRICE, ITEM_STOCK) 
-		VALUES ('$var_Id','$item_id','$variantDesc', '$price','$stock');";
+
+require 'upload.php';
+
+$query2 = "INSERT INTO ITEM_VARIANT(VARIANT_ID,ITEM_ID,VARIANT_DESC, PRICE, ITEM_STOCK, ITEM_IMG) 
+		VALUES ('$var_Id','$item_id','$variantDesc','$price','$stock','$target_file');";
 		$result = mysqli_query($conn, $query2);
 		if ($result){
 			$_SESSION["check"]=true;
@@ -53,41 +59,39 @@ mysqli_close($conn);
 ?>
 <!DOCTYPE html>	
 <html>
-	<form action="addvariant.php" method="Post">
-				
-				Item name: <?php echo $itemName; 
+	Item name: <?php echo $itemName; 
 				?><br>	
 				<br>
-				Item Description: <?php echo $itemDesc; 
+	Item Description: <?php echo $itemDesc; 
 				?><br>
 				<p></p>
-			</form>
-	<form id="vars" action="addvariant.php" method="Post">
+	<form id="adimAdd" action="addvariant.php" method="Post" enctype="multipart/form-data">
 	Variant*:<br>
-	<input type="text" name="variantDesc" value="<?php
+	<input required type="text" name="variantDesc" maxlength = "140" value="<?php
 			if(isset($_POST['variantDesc'])){
 			echo $_POST['variantDesc'];
 			}
-		?>"><br>
-		
+		?>">
+	<br>
 	Price*:<br>
-	<input type="text" name="price"value = "<?php
-			if(isset($_POST['price'])){
-			echo $_POST['price'];
-			}
-		?>"><br>
+	<input required type="number" step=".01" decimals="1" name="price" maxlength = "10" value = "<?php
+	if(isset($_POST['price'])){
+	echo $_POST['price'];
+	}
+	?>"><br>
 	
 	Initial variant Stock*:<br>
-	<input type="text" name="stock"value = "<?php
-			if(isset($_POST['stock'])){
-			echo $_POST['stock'];
-			}
-		?>"><br> 
+	<input required type="number" step="10" name="stock" maxlength = "7" value = "<?php
+	if(isset($_POST['stock'])){
+	echo $_POST['stock'];
+	}
+	?>"><br> 
 	
 	<p></p>
 	Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="file" name="fileToUpload1" id="fileToUpload1">
 	<p></p>
-	<button type="submit" name = "variantConfirm"> Add Variant</button>
-		</form>
+	<button type="submit" name = "confirm"> Add Product</button>
+
+    </form>
 </html>
