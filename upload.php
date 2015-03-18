4d36505779601adfab5d557edf35a36219ab5e09
Reece Tucker <reecetucker2@gmail.com>
2015-03-18 14:48:37 +0000

    upload.php edited online with Bitbucket

--- upload.php
+++ upload.php
@@ -1,15 +1,18 @@
 <?php
-require($_SERVER['DOCUMENT_ROOT'] . '/php-eshop/src/require.php');
-
-$conn = Common::connect_db();
-
-$target_dir = $_SERVER['DOCUMENT_ROOT'] ."/img/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+	require($_SERVER['DOCUMENT_ROOT'] . '/assignment2/src/require.php');	
+	$conn = Common::connect_db();
+	if($count > 0)
+	{
+	
+$target_dir = "/var/www/html/assignment2/images/";
+$file_to_upload = "fileToUpload".$count;
+$target_file = $target_dir . basename($_FILES["$file_to_upload"]["name"]);
+if($target_file != $target_dir){
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    $check = getimagesize($_FILES["$file_to_upload"]["tmp_name"]);
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
@@ -24,13 +27,13 @@ if (file_exists($target_file)) {
     $uploadOk = 0;
 }
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
+if ($_FILES["$file_to_upload"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif" ) {
+&& $imageFileType != "gif" ) {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
 }
@@ -39,14 +42,14 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-        //some SQL to add file path to varient table
-
-
-    } else {
-        echo "Sorry, there was an error uploading your file.";
+    if (move_uploaded_file($_FILES["$file_to_upload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["$file_to_upload"]["name"]). " has been uploaded.";
+		
+    } else 
+      echo "Sorry, there was an error uploading your file.";
     }
+}
+$target_file = "images/". basename($_FILES["$file_to_upload"]["name"]);
 }
 
 ?>
